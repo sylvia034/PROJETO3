ea54e7b35a5cbda9151feba4a38cc977984fdd98
sylvia034 <00001108862251sp@al.educacao.sp.gov.br>
2024-11-14 12:26:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Foi Pedro Álvares Cabral'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto que é 9 x 8?',
+    '72'
+)
+
+criaCartao(
+    'geografia',
+    'Quantos estados existem no Brasil?',
+    'Existem 26 estados'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz boa noite em Inglês?',
+    'Boa noite em ingles é Goodnight'
+)
